4e01a11ed0d4be39a10a2605094ebed48ff2ce03
eliogrc <elioenaig041294@gmail.com>
2025-01-30 14:01:43 -0600

    Refactor Bookmarks component to use react-query for data fetching

--- app/bookmarks/page.tsx
+++ app/bookmarks/page.tsx
@@ -1,23 +1,60 @@
+"use client"
+
+import { useQuery } from '@tanstack/react-query'
+// import { useState, useEffect } from "react"
 import { Heading, Text } from "@chakra-ui/react"
 
 import { Bookmark } from "@/components/bookmark"
 // import { BookmarkType } from "./schema"
-import { orm } from "./db"
+// import { orm } from "./db"
+import { BookmarkType } from "./schema"
 
-export default async function Bookmarks() {
-
-    /*
+export default function Bookmarks() {
+  /*
     NOTA. Esta es una forma de consumir datos desde un server component pero no es recomendable usarlo en producción.
-
     const bookmarksResponse = await fetch("http://localhost:3000/bookmarks/api")
     const bookmarks = (await bookmarksResponse.json()) as { data: BookmarkType[] };*/
 
+  /*
+  Nota. Esta es una forma de consumir datos desde un server component usando un ORM,se suele ocupar en prod pero no es muy común.
   const bookmarks = await orm.query.bookmarks.findMany({
     limit: 10,
     with: {
       author: true,
     },
+  })*/
+
+    /*
+  Nota. Forma de consumir datos desde un client component con la tipica useEffect
+  const [bookmarks, setBookmarks] = useState<BookmarkType[]>([])
+  const [status, setStatus] = useState<'idle'|'loading'|'error'|'success'>('idle')
+
+  useEffect(() => {
+    setStatus('loading')
+    fetch("/bookmarks/api", {
+      next: { tags: ["bookmarks"] },
+    })
+      .then((response) => response.json())
+      .then(({ data }) => {
+        setBookmarks(data)
+        setStatus('success')
+      })
+  }, [])*/
+
+  const {data: bookmarks, status } = useQuery({
+    queryKey: ['bookmarks'],
+    queryFn: async () => {
+      return fetch("/bookmarks/api", {
+        next: { tags: ["bookmarks"] },
+      })
+        .then((response) => response.json())
+        .then(({ data }) => {
+          return data as BookmarkType[]
+        })
+    }
   })
+
+  console.log({ status })
 
   return (
     <main className="mt-12">
